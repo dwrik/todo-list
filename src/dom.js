4b86539deb12fad62bd8afe8eb7f71f83ac5bb5e
Wrik Das <wrikdas22@gmail.com>
2021-03-14 18:25:43 +0530

    Added openMenu, closeMenu and toggleProjects modules

--- src/dom.js
+++ src/dom.js
@@ -1,11 +1,11 @@
-import { TodoElement } from './todo';
+import { todoList, TodoElement } from './todo';
 
 const projectTodos = document.querySelector('.project-todos');
 
-const renderList = (todoList) => {
+const renderList = () => {
 
     const removeExistingList = () => {
-        const todos = document.querySelectorAll('.todo-container');
+        const todos = [ ...document.querySelectorAll('.todo-container') ];
         todos.forEach(todo => projectTodos.removeChild(todo));
     };
 
@@ -18,4 +18,28 @@ const renderList = (todoList) => {
 
 };
 
-export { renderList };
+const openMenu = () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.width = '100%';
+};
+
+const closeMenu = () => {
+    const sidebar = document.querySelector('.sidebar');
+    sidebar.style.removeProperty('width');
+};
+
+const toggleProjects = () => {
+    const sidebarVariableItems = document.querySelector('.sidebar-variable-items');
+    const projects = document.querySelector('.projects');
+    const footer = document.querySelector('.footer');
+
+    if (projects.style.maxHeight) {
+        projects.style.overflow = 'hidden';
+        projects.style.maxHeight = null;
+    } else {
+        projects.style.overflow = 'auto';
+        projects.style.maxHeight = `calc(${sidebarVariableItems.scrollHeight}px - ${footer.scrollHeight}px)`;
+    }
+}
+
+export { renderList, openMenu, closeMenu, toggleProjects };
