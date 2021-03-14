dd5bd8fe514cd54a975ba0ac74b9efad49e07fb2
Wrik Das <wrikdas22@gmail.com>
2021-03-14 18:28:09 +0530

    Added event listeners for menu button, close button and projects tab

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import { todoList, TodoElement } from './todo';
-import { renderList } from './dom';
+import { renderList, openMenu, closeMenu, toggleProjects } from './dom';
 
-renderList(todoList, TodoElement);
+renderList();
+
+const projectsTab = document.querySelector('#projects-tab');
+projectsTab.addEventListener('click', toggleProjects);
+
+const menuButton = document.querySelector('#menu-button');
+menuButton.addEventListener('click', openMenu);
+
+const closeButton = document.querySelector('#close-button');
+closeButton.addEventListener('click', closeMenu);
