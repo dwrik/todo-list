9e51d7adc7a94e81f9ee355a49c9fdaa827c4371
Wrik Das <wrikdas22@gmail.com>
2021-03-15 21:01:13 +0530

    Implemented adding of todo to the list

--- src/todo.js
+++ src/todo.js
@@ -1,3 +1,4 @@
+// Todo factory
 const Todo = (title, description, dueDate, priority) => {
     return {
         title,
@@ -7,7 +8,7 @@ const Todo = (title, description, dueDate, priority) => {
     };
 };
 
-let todoList = [];
+export const todoList = [];
 
 const PRIORITY_LOW = 'low';
 const PRIORITY_MEDIUM = 'medium';
@@ -16,8 +17,22 @@ const PRIORITY_HIGH = 'high';
 const lorem = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta veniam facere nisi voluptatibus cumque fugiat. Repudiandae itaque at adipisci aspernatur et veritatis blanditiis a, quia suscipit recusandae non, aut ad!';
 
 todoList.push(Todo('Dog food', lorem, null, PRIORITY_HIGH));
-todoList.push(Todo('Buy whiteboard and accessories', 'Preferrably 3x4 and markers', '28 Mar 2021', PRIORITY_LOW));
-todoList.push(Todo('Get groceries', lorem, '15 Apr 2021', PRIORITY_MEDIUM));
+todoList.push(Todo('Buy whiteboard and accessories', 'Preferrably 3x4 and markers', '2021.03.28', PRIORITY_LOW));
+todoList.push(Todo('Get groceries', lorem, '2021.04.15', PRIORITY_MEDIUM));
 
+export const addTodo = (formData) => {
+    const title = formData.get('title');
+    const priority = formData.get('priority');
+    const description = formData.get('description');
+    const date = (formData.get('date'))? formData.get('date') : null;
 
-export { todoList };
+    todoList.push(
+        Todo(
+            title,
+            description,
+            date,
+            priority
+        ));
+
+    console.log(todoList);
+};
