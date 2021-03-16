2f1f69c85e63cf47221335e8938dbeb4d18694b3
Wrik Das <wrikdas22@gmail.com>
2021-03-16 22:16:45 +0530

    Implemented sidebar tabs using tab object and showTab method, heavily refactored renderList code, added comments

--- src/dom.js
+++ src/dom.js
@@ -1,33 +1,84 @@
-import { format } from 'date-fns';
+import { format, isToday } from 'date-fns';
+import isAfter from 'date-fns/isAfter';
 import { changePriority, deleteTodo } from './todo';
 
-export const renderList = (todoList) => {
+// sidebar tabs
+export const tab = {
+    INBOX: 'Inbox',
+    TODAY: 'Today',
+    UPCOMING: 'Upcoming',
+    CURRENT: 'Inbox',
+};
+
+// renders todo list based on selected sidebar tab
+export const renderList = (currTab, todoList) => {
     const projectTodos = document.querySelector('.project-todos');
+    tab.CURRENT = currTab;
 
-    const removeExistingList = () => {
-        const todos = [ ...document.querySelectorAll('.todo-container') ];
-        todos.forEach(todo => projectTodos.removeChild(todo));
-    };
+    // remove existing list elements
+    const todos = [ ...document.querySelectorAll('.todo-container') ];
+    todos.forEach(todo => projectTodos.removeChild(todo));
 
-    removeExistingList();
+    let filteredList;
 
-    for (let index in todoList) {
-        const todoElement = getTodoElement(todoList, todoList[index]);
+    // filter list based on tab
+    switch(tab.CURRENT) {
+        case tab.INBOX:
+            filteredList = todoList;
+            break;
+
+        case tab.TODAY:
+            filteredList = todoList.filter(todo =>
+                isToday(
+                    new Date(todo.dueDate)
+                ));
+            break;
+
+        case tab.UPCOMING:
+            filteredList = todoList.filter(todo =>
+                isAfter(
+                    new Date(todo.dueDate),
+                    new Date()
+                ))
+                filteredList.sort((a, b) =>
+                    new Date(a.dueDate).getTime() - new Date(b.dueDate).getTime());
+            break;
+    }
+
+    // add filtered list elements to DOM
+    for (let index in filteredList) {
+        const todoElement = getTodoElement(todoList, filteredList[index]);
         projectTodos.appendChild(todoElement);
     }
 
 };
 
+// update project header and renders todo list
+export const showTab = (tab, todoList) => {
+    const sidebar = document.querySelector('.sidebar');
+    if (sidebar.style.width === '100%') {
+        closeMenu();
+    }
+
+    const projectName = document.querySelector('.project-name');
+    projectName.innerHTML = tab;
+
+    renderList(tab, todoList);
+};
+
+// open sidebar
 export const openMenu = () => {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.width = '100%';
 };
 
+// close sidebar
 export const closeMenu = () => {
     const sidebar = document.querySelector('.sidebar');
     sidebar.style.removeProperty('width');
 };
 
+// toggle custom project list
 export const toggleProjects = () => {
     const sidebarVariableItems = document.querySelector('.sidebar-variable-items');
     const expandProjects = document.querySelector('#expand-projects');
@@ -45,6 +96,7 @@ export const toggleProjects = () => {
     }
 }
 
+// hides modal and resets form
 export const hideModal = () => {
     const modal = document.querySelector('.modal');
     modal.style.display = 'none';
@@ -66,60 +118,33 @@ export const hideModal = () => {
 };
 
 
+// view modal
 export const showModal = () => {
     const modal = document.querySelector('.modal');
     modal.style.display = 'flex';
 };
 
-export const getTodoElement = (todoList, todoObject) => {
+// private helper for creating todo dom element
+const getTodoElement = (todoList, todoObject) => {
 
     // todo container : todo + todo details
     const todoContainer = document.createElement('div');
     todoContainer.classList.add('todo-container');
 
-    // todo : icon + todo content
+    // todo : checkbox + todo content
     const todo = document.createElement('div');
     todo.classList.add('todo');
-
 
     const checkbox = document.createElement('span');
     checkbox.classList.add('material-icons');
     checkbox.id = `checkbox-${todoObject.id}`;
 
-    checkbox.addEventListener('click', (event) => {
-        checkbox.classList.toggle('grey');
-        checkbox.classList.toggle('blue');
-
-        checkbox.parentElement.classList.toggle('checked');
-        checkbox.parentElement.nextSibling.classList.toggle('checked');
-
-        checkbox.innerHTML = (checkbox.innerHTML === 'check_circle')?
-                'radio_button_unchecked' : 'check_circle';
-
-        todoObject.isChecked = (todoObject.isChecked)? false : true;
-    });
-
-
+    // todo content : todo body + actions
     const todoContent = document.createElement('div');
     todoContent.classList.add('todo-content');
 
-    const toggleDetails = (event) => {
-        const todoDetails = document.querySelectorAll('.todo-details')[todoObject.id];
-        const todo = todoDetails.previousSibling;
-
-        todo.classList.toggle('border-hide');
-        todoDetails.classList.toggle('visibility');
-
-        if (todoDetails.style.maxHeight) {
-            todoDetails.style.maxHeight = null;
-        } else {
-            todoDetails.style.maxHeight = todoDetails.scrollHeight + "px";
-        }
-    };
-
     const todoBody = document.createElement('div');
     todoBody.classList.add('todo-body');
-    todoBody.addEventListener('click', toggleDetails);
 
     const title = document.createElement('label');
     title.htmlFor = `checkbox-${todoObject.id}`;
@@ -127,6 +152,7 @@ export const getTodoElement = (todoList, todoObject) => {
 
     todoBody.appendChild(title);
 
+    // add date if present
     if (todoObject.dueDate) {
         const dueDate = document.createElement('p');
         dueDate.classList.add('duedate');
@@ -137,6 +163,7 @@ export const getTodoElement = (todoList, todoObject) => {
         todoBody.appendChild(dueDate);
     }
 
+    // actions (edit, priority, delete)
     const actions = document.createElement('div');
     actions.classList.add('actions');
 
@@ -144,59 +171,13 @@ export const getTodoElement = (todoList, todoObject) => {
     editIcon.classList.add('material-icons-outlined', 'md-20', 'darkgrey');
     editIcon.innerHTML = 'edit';
 
-    // edit todo
-    editIcon.addEventListener('click', (event) => {
-        showModal();
-
-        const modalHeading = document.querySelector('.modal-heading');
-        modalHeading.innerHTML = 'Edit Todo';
-
-        const operation = document.querySelector('#operation');
-        operation.value = 'update';
-
-        const id = document.querySelector('input[name="id"]');
-        id.value = todoObject.id;
-
-        const title= document.querySelector('.modal-input[name="title"]');
-        title.value = todoObject.title;
-
-        const description= document.querySelector('.modal-input[name="description"]');
-        description.value = todoObject.description;
-
-        const date = document.querySelector('.flatpickr.modal-input')._flatpickr;
-        date.setDate(todoObject.dueDate);
-
-        const priority = document.querySelector('.modal-input[name="priority"]');
-        priority.value = todoObject.priority;
-    });
-
     const flagIcon = document.createElement('span');
     flagIcon.classList.add('material-icons', 'md-20', `priority-${todoObject.priority}`);
     flagIcon.innerHTML = 'flag';
 
-    // toggle priority, low -> med -> high
-    flagIcon.addEventListener('click', (event) => {
-        flagIcon.classList.remove(`priority-${todoObject.priority}`)
-
-        switch (todoObject.priority) {
-            case 'low':    changePriority(todoObject.id, 'medium'); break;
-            case 'medium': changePriority(todoObject.id, 'high');   break;
-            case 'high':   changePriority(todoObject.id, 'low');    break;
-        }
-
-        flagIcon.classList.add(`priority-${todoObject.priority}`)
-        renderList(todoList);
-    });
-
     const deleteIcon = document.createElement('span');
     deleteIcon.classList.add('material-icons-outlined', 'md-20', 'darkgrey');
     deleteIcon.innerHTML = 'delete';
-
-    // delete todo
-    deleteIcon.addEventListener('click', (event) => {
-        deleteTodo(todoObject.id);
-        renderList(todoList);
-    });
 
     actions.appendChild(editIcon);
     actions.appendChild(flagIcon);
@@ -208,11 +189,10 @@ export const getTodoElement = (todoList, todoObject) => {
     todo.appendChild(checkbox);
     todo.appendChild(todoContent);
 
-    // todo details
+    // todo details : description + other details (due date & priority)
     const todoDetails = document.createElement('div');
     todoDetails.classList.add('todo-details');
-
-    todoDetails.addEventListener('click', toggleDetails);
+    todoDetails.id = `todo-detail-${todoObject.id}`;
 
     const description = document.createElement('div');
     description.classList.add('description');
@@ -235,8 +215,8 @@ export const getTodoElement = (todoList, todoObject) => {
     date.appendChild(dateHeading);
 
     const dueDate = (todoObject.dueDate)?
-            format(new Date(todoObject.dueDate), 'dd.MM.yyyy')
-            : 'N/A';
+            format(new Date(todoObject.dueDate), 'dd.MM.yyyy') : 'N/A';
+
     dateHeading.after(dueDate);
 
     const priority = document.createElement('div');
@@ -265,9 +245,83 @@ export const getTodoElement = (todoList, todoObject) => {
     todoContainer.appendChild(todo);
     todoContainer.appendChild(todoDetails);
 
-    // set todo checked status
-    // after it has been attached
-    // to DOM to avoid TypeError
+    // event listeners :
+    // todoBody, todoDetails, checkbox, edit, flag, delete
+
+    const toggleDetails = (event) => {
+        const todoDetails = document.querySelector(`#todo-detail-${todoObject.id}`);
+        const todo = todoDetails.previousSibling;
+
+        todo.classList.toggle('border-hide');
+        todoDetails.classList.toggle('visibility');
+
+        if (todoDetails.style.maxHeight) {
+            todoDetails.style.maxHeight = null;
+        } else {
+            todoDetails.style.maxHeight = todoDetails.scrollHeight + "px";
+        }
+    };
+
+    // toggle details using todo body/details
+    todoBody.addEventListener('click', toggleDetails);
+    todoDetails.addEventListener('click', toggleDetails);
+
+    // mark todo as checked/unchecked
+    checkbox.addEventListener('click', (event) => {
+        checkbox.classList.toggle('grey');
+        checkbox.classList.toggle('blue');
+
+        checkbox.parentElement.classList.toggle('checked');
+        checkbox.parentElement.nextSibling.classList.toggle('checked');
+
+        checkbox.innerHTML = (checkbox.innerHTML === 'check_circle')?
+                'radio_button_unchecked' : 'check_circle';
+
+        todoObject.isChecked = (todoObject.isChecked)? false : true;
+    });
+
+    // edit todo
+    editIcon.addEventListener('click', (event) => {
+        showModal();
+
+        const modalHeading = document.querySelector('.modal-heading');
+        modalHeading.innerHTML = 'Edit Todo';
+
+        const operation = document.querySelector('#operation');
+        operation.value = 'update';
+
+        const id = document.querySelector('input[name="id"]');
+        id.value = todoObject.id;
+
+        const title= document.querySelector('.modal-input[name="title"]');
+        title.value = todoObject.title;
+
+        const description= document.querySelector('.modal-input[name="description"]');
+        description.value = todoObject.description;
+
+        const date = document.querySelector('.flatpickr.modal-input')._flatpickr;
+        date.setDate(todoObject.dueDate);
+
+        const priority = document.querySelector('.modal-input[name="priority"]');
+        priority.value = todoObject.priority;
+    });
+
+    // change priority
+    flagIcon.addEventListener('click', (event) => {
+        flagIcon.classList.remove(`priority-${todoObject.priority}`)
+        changePriority(todoObject.id);
+        flagIcon.classList.add(`priority-${todoObject.priority}`)
+        renderList(tab.CURRENT, todoList);
+    });
+
+    // delete todo
+    deleteIcon.addEventListener('click', (event) => {
+        deleteTodo(todoObject.id);
+        renderList(tab.CURRENT, todoList);
+    });
+
+    // set todo checked status after it has been
+    // attached to DOM to avoid TypeError 
     if (todoObject.isChecked) {
         checkbox.classList.add('blue');
         checkbox.parentElement.classList.add('checked');
