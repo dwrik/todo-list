26086ad3162696bfdd9c4c9beb31371f82b327b3
Wrik Das <wrikdas22@gmail.com>
2021-03-17 14:11:22 +0530

    Added event listeners for new project modal and updated code for today and upcoming tabs to consider all todos from all projects before filtering them

--- src/index.js
+++ src/index.js
@@ -14,7 +14,16 @@ flatpickr('.flatpickr', {
 });
 
 // initial rendering of list
-dom.renderList(dom.tab.INBOX, todo.todoList);
+dom.renderList(dom.tab.INBOX, todo.projects['Inbox']);
+dom.renderProjectsList(todo.projects);
+
+const tabs = [ ...document.querySelectorAll('.tab') ];
+tabs.forEach(tab => tab.addEventListener('click', (event) => {
+    tabs.forEach(t => t.classList.remove('selected'));
+    if (tab === event.target) {
+        tab.classList.add('selected');
+    }
+}));
 
 // inbox tab
 const inboxTab = document.querySelector('#inbox-tab');
@@ -25,13 +34,23 @@ inboxTab.addEventListener('click', (event) => {
 // today tab
 const todayTab = document.querySelector('#today-tab');
 todayTab.addEventListener('click', (event) => {
-    dom.showTab(dom.tab.TODAY, todo.todoList)
+    dom.showTab(
+        dom.tab.TODAY,
+        Object.getOwnPropertyNames(todo.projects)
+        .map(key => todo.projects[key])
+        .reduce((prev, curr) => prev.concat(curr)
+        ));
 });
 
 // upcoming tab
 const upcomingTab = document.querySelector('#upcoming-tab');
 upcomingTab.addEventListener('click', (event) => {
-    dom.showTab(dom.tab.UPCOMING, todo.todoList);
+    dom.showTab(
+        dom.tab.UPCOMING,
+        Object.getOwnPropertyNames(todo.projects)
+        .map(key => todo.projects[key])
+        .reduce((prev, curr) => prev.concat(curr)
+        ));
 });
 
 // toggle project menu
@@ -48,11 +67,11 @@ closeMenu.addEventListener('click', dom.closeMenu);
 
 // add todo form
 const addButton = document.querySelector('.add-button');
-addButton.addEventListener('click', dom.showModal);
+addButton.addEventListener('click', dom.showTodoModal);
 
 // cancel add todo
-const closeModal = document.querySelector('#modal-cancel');
-closeModal.addEventListener('click', dom.hideModal);
+const cancelTodo = document.querySelector('#todo-modal-cancel');
+cancelTodo.addEventListener('click', dom.hideTodoModal);
 
 // submit add todo form
 const todoForm = document.querySelector('#todo-form');
@@ -66,14 +85,50 @@ todoForm.addEventListener('submit', (event) => {
         case 'update': todo.updateTodo(formData); break;
     }
 
-    dom.renderList(dom.tab.CURRENT, todo.todoList);
-    dom.hideModal();
+    if (dom.tab.CURRENT === dom.tab.TODAY || dom.tab.CURRENT === dom.tab.UPCOMING) {
+        dom.renderList(
+            dom.tab.CURRENT,
+            Object.getOwnPropertyNames(todo.projects)
+            .map(key => todo.projects[key])
+            .reduce((prev, curr) => prev.concat(curr)
+            ));
+    } else {
+        dom.renderList(
+            dom.tab.CURRENT,
+            todo.projects[dom.tab.CURRENT]
+            );
+    }
+
+    dom.hideTodoModal();
 });
 
-// hide modal on click
-const modal = document.querySelector('.modal');
+// new project form
+const newProject = document.querySelector('#add-project');
+newProject.addEventListener('click', dom.showProjectModal);
+
+// cancel add project
+const cancelProject = document.querySelector('#project-modal-cancel');
+cancelProject.addEventListener('click', dom.hideProjectModal);
+
+// submit add project form
+const projectForm = document.querySelector('#project-form');
+projectForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const formData = new FormData(projectForm);
+    todo.addProject(formData);
+    dom.renderProjectsList(todo.projects);
+    dom.hideProjectModal();
+});
+
+// hide modals on click
+const todoModal = document.querySelector('.modal#todo-modal');
+const projectModal = document.querySelector('.modal#project-modal');
 window.addEventListener('click', (event) => {
-    if (event.target == modal) {
-        dom.hideModal();
+    if (event.target === todoModal) {
+        dom.hideTodoModal();
+    }
+
+    if (event.target === projectModal) {
+        dom.hideProjectModal();
     }
 });
